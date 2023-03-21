ec779deb07077e1785f96cef9e58d39fc537d542
Alan Hindryckx <dgequis@gmail.com>
2023-03-21 20:20:44 -0300

    creando mi primer repositorio en git

---
+++ script.js
@@ -0,0 +1,6 @@
+const listaAlumnos = ["gonzalo", "pablo", "micaela", "carola"]
+
+for (let i = 0; i < listaAlumnos.length; i = i + 1) {
+    let alumno = listaAlumnos[i]
+    console.log("Buenas noches, mi nombre es " + alumno)
+}
